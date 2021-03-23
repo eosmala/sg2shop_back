21407ede055f0e2303f7a36d2e6750412f24b860
sHautala <sjhautala98@gmail.com>
2021-03-23 16:44:19 +0200

    create base database, headers, functions

---
+++ database.sql
@@ -0,0 +1,12 @@
+drop database if exists webshop;
+create database webshop;
+use webshop;
+
+create table category (
+    id int primary key auto_increment,
+    name varchar(50) not null
+);
+
+insert into category(name) value ('Kissat');
+insert into category(name) value ('Koirat');
+
