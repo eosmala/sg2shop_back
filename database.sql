217f89ce8733c4e25fcfb7ff245e4f234388f415
Elina Osmala <n0osel00@students.oamk.fi>
2021-03-30 12:53:19 +0300

    Add product table

--- database.sql
+++ database.sql
@@ -11,3 +11,18 @@ insert into category(name) value ('Koirat');
 insert into category(name) value ('Kissat');
 insert into category(name) value ('Pieneläimet');
 insert into category(name) value ('Tarjoukset');
+
+// 30.3.2021
+create table product (
+    id int primary key auto_increment,
+    name varchar(100) not null,
+    price double (10,2) not null,
+    image varchar(50),
+    category_id int not null,
+    index category_id(category_id),
+    foreign key (category_id) references category(id)
+    on delete restrict
+);
+
+insert into product (name, price, category_id) values ('raapimapuu', 15, 2);
+insert into product (name, price, category_id) values ('hihna', 10, 1);
