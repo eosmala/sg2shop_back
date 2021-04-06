19ec6fc0e0e473f6a12cf2634d982723252310e9
Valtteri Hendriksson <n9heva00@students.oamk.fi>
2021-04-06 13:05:04 +0300

    getproductinfo

---
+++ products/getproductinfo.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+// read parameters from url
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+
+// parameters are separated with a slash
+$parameters = explode('/', $uri);
+
+// category id is first parameter so it follows after address  separated with slash
+$productId = $parameters[1];
+
+try {
+    $db = openDb();
+    selectAsJson($db, "select * from product where id = $productId");
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
+?>
