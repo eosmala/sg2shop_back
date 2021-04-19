a7b0420794318d7894d00c6c1a97598192f08ccb
Elina Osmala <n0osel00@students.oamk.fi>
2021-04-19 12:55:35 +0300

    Combined everything and added a stockamounts

--- database.sql
+++ database.sql
@@ -12,23 +12,21 @@ insert into category(name) value ('Kissat');
 insert into category(name) value ('Pieneläimet');
 insert into category(name) value ('Tarjoukset');
 
--- 30.3.2021
 create table product (
     id int primary key auto_increment,
     name varchar(100) not null,
     price double (10,2) not null,
-    amount int,
+    stock_amount int,
     image varchar(50),
     category_id int not null,
+    description varchar(255),
     index category_id(category_id),
+    subcategory_id INT,
+    FOREIGN KEY (subcategory_id)
+    REFERENCES subcategory(sub_id),
     foreign key (category_id) references category(id)
     on delete restrict
 );
-
---8.4.
-ALTER TABLE product
-ADD COLUMN description varchar(255),
-ADD COLUMN subcategory_id INT;
 
 
 
@@ -37,38 +35,31 @@ create table subcategory (
     name varchar(50) not null
 );
 
-ALTER TABLE product
-ADD CONSTRAINT sub_id
-FOREIGN KEY (subcategory_id)
-    REFERENCES subcategory(sub_id);
 
 -- Lisää tuotteita, kuvat, descriptionit jne. 09.04.2021 --
 
 -- koirat
-insert into product (name, amount, price, image, category_id, description) values ('Hihna', 13, 16.99, "hihna.png", 1, 'Remmi koirasi ulkoilutukseen.');
-insert into product (name, amount, price, image, category_id, description) values ('Ruokanappulat', 5, 19.90, "ruokanappulat.png", 1, 'Herkullisia rehunappeja koirallesi.');
-insert into product (name, amount, price, image, category_id, description) values ('Tennispallo', 25, 2.00, "tennispallo.png", 1, 'Loputonta leikin iloa koirallesi.');
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Hihna', 13, 16.99, "hihna.png", 1, 'Remmi koirasi ulkoilutukseen.', 50);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Ruokanappulat', 5, 19.90, "ruokanappulat.png", 1, 'Herkullisia rehunappeja koirallesi.', 300);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Tennispallo', 25, 2.00, "tennispallo.png", 1, 'Loputonta leikin iloa koirallesi.', 29);
 
 -- kissat
-insert into product (name, amount, price, image, category_id, description) values ('Raapimapuu', 3, 79.90, "raapimapuu.png", 2, 'Kissan unelma raapimishommiin.');
-insert into product (name, amount, price, image, category_id, description) values ('Laserosoitin', 9, 5.90, "laserosoitin.png", 2, 'Laita kissasi jahtaamaan punaista valoa.');
-insert into product (name, amount, price, image, category_id, description) values ('Peti', 10, 12.90, "peti.png", 2, 'Muhkea peti kissan loikoiluun.');
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Raapimapuu', 3, 79.90, "raapimapuu.png", 2, 'Kissan unelma raapimishommiin.', 50);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Laserosoitin', 9, 5.90, "laserosoitin.png", 2, 'Laita kissasi jahtaamaan punaista valoa.', 80);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Peti', 10, 12.90, "peti.png", 2, 'Muhkea peti kissan loikoiluun.', 124);
 
 -- pieneläimet
-insert into product (name, amount, price, image, category_id, description) values ('Terraario', 2, 120.00, "terraario.png", 3, 'Terraario käärmeelle tai mille keksitkään.');
-insert into product (name, amount, price, image, category_id, description) values ('Siemenet', 33, 4.90, "siemenet.png", 3, 'Herkullisia siemeniä lintuystävillesi.');
-insert into product (name, amount, price, image, category_id, description) values ('Tekokasvi', 12, 5.00, "tekokasvi.png", 3, 'Upea tekokasvi akvaarioon.');
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Terraario', 2, 120.00, "terraario.png", 3, 'Terraario käärmeelle tai mille keksitkään.', 154);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Siemenet', 33, 4.90, "siemenet.png", 3, 'Herkullisia siemeniä lintuystävillesi.', 447);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Tekokasvi', 12, 5.00, "tekokasvi.png", 3, 'Upea tekokasvi akvaarioon.', 186);
 
 -- tarjoukset
-insert into product (name, amount, price, image, category_id, description) values ('Kissanhiekka', 28, 5.90, "kissanhiekka.png", 4, 'Mieluisat hiekat kissojen tarpeiden tekoon.');
-insert into product (name, amount, price, image, category_id, description) values ('Mega-puruluu', 16, 2.50, "puruluu.png", 4, 'Pureskeltavaa jopa isommalle koiralle.');
-insert into product (name, amount, price, image, category_id, description) values ('Kalanruoka', 8, 3.90, "kalanruoka.png", 4, 'Maukasta apetta kalakavereille.');
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Kissanhiekka', 28, 5.90, "kissanhiekka.png", 4, 'Mieluisat hiekat kissojen tarpeiden tekoon.', 458);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Mega-puruluu', 16, 2.50, "puruluu.png", 4, 'Pureskeltavaa jopa isommalle koiralle.', 475);
+insert into product (name, amount, price, image, category_id, description, stock_amount) values ('Kalanruoka', 8, 3.90, "kalanruoka.png", 4, 'Maukasta apetta kalakavereille.', 288);
 
 --Customer ja registration tablet yhdistetty
 
-use webshop;
-
-drop table if exists customer;
 create table customer (
   	id INT primary key AUTO_INCREMENT,
     username varchar(100) NOT NULL,
@@ -84,6 +75,3 @@ create table customer (
     phonenumber varchar(20)
 );
 
--- 19.4.21
-ALTER TABLE product
-ADD COLUMN stock_amount INT;
